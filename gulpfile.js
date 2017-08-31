89fddb74a5071b710798308806b33fbc3e83500a
wanbo.ge <gewanbo@gmail.com>
2017-08-31 16:22:20 +0800

    Setting gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
-gulp.task('default', ['check'], function(cb) {
+gulp.task('default', ['script'], function(cb) {
     console.log('This is default task.');
 
     return gulp.src('js/analytics-source.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(rename({basename: 'analytics', suffix: '.min'}))
+    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('test/js'));
 });
 
+gulp.task('script', function(cb) {
+
+    return gulp.src('js/script.js')
+    .pipe(uglify())
+    .pipe(rename({basename: 'script', suffix: '.min'}))
+    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('test/js'));
+});
 
 gulp.task('check', function(cb) {
 
